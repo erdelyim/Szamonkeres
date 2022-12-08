9396cbd3c51a9f552578c6e5f23c9ca31843d6fc
erdelyim <90777816+erdelyim@users.noreply.github.com>
2022-12-08 10:58:33 +0100

    Add files via upload

---
+++ pyhton.py
@@ -0,0 +1,22 @@
+"""
+nev1 = input("kérem a vezetékneved")
+nev2 = input("kérem a keresztneved")
+print("Üdvözöllek")
+print(nev1,nev2)
+"""
+szam1 = int(input("kérek egy egész számot:"))
+print("A mgelőző szám:",(szam1 -1))
+print("A rákövetkező szám",(szam1 +1))
+
+
+"""
+szam1 = input("Kérem az első számot 1-100 között:")
+szam2 = input("Kérem a második számot 1-100 között:")
+szam1 = int(szam1)
+szam2 = int(szam2)
+
+print("A két szám összege", szam1, "+", szam2, "=", szam1 + szam2)
+print("A két szám különbsége", szam1, "-", szam2, "=", szam1 - szam2)
+print("A két szám szorzata", szam1, "x", szam2, "=", szam1 * szam2)
+print("A két szám hányadosa",szam1, "/", szam2, "=", szam1 / szam2)
+"""
